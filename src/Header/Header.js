9d9d4d25a58a024cbbd36b6af96cfd20ec5f7db4
wrunion <winterrunion@gmail.com>
2020-04-27 19:58:17 -0700

    Add header styling.

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header id="Header" className="grid-component">
+    <header className="Header grid-component">
       <h1>Winter's Flash Cards</h1>
       <h2>Pick a language below to learn new animal names!</h2>
     </header>
