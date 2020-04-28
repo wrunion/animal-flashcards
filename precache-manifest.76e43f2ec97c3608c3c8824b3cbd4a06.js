dfc6bc2401a0a4401d465694b873864c8ab15bfb
wrunion <winterrunion@gmail.com>
2020-04-27 20:36:41 -0700

    Updates

---
+++ precache-manifest.76e43f2ec97c3608c3c8824b3cbd4a06.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "828bcedd89ed41acc4f80f75695303f0",
+    "url": "/animal-flashcards/index.html"
+  },
+  {
+    "revision": "54e047af213b21750e75",
+    "url": "/animal-flashcards/static/css/main.8e53cb66.chunk.css"
+  },
+  {
+    "revision": "09df50c37840235b696a",
+    "url": "/animal-flashcards/static/js/2.b04793e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/animal-flashcards/static/js/2.b04793e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54e047af213b21750e75",
+    "url": "/animal-flashcards/static/js/main.8644e6e4.chunk.js"
+  },
+  {
+    "revision": "a2f779e2f6ee9fde64ff",
+    "url": "/animal-flashcards/static/js/runtime-main.e9d79776.js"
+  },
+  {
+    "revision": "ae959a2bf164cba57ef334d6fdafeef9",
+    "url": "/animal-flashcards/static/media/alligator.ae959a2b.png"
+  },
+  {
+    "revision": "0fc83bdaf09b7b5caf030072aaf094a2",
+    "url": "/animal-flashcards/static/media/elephant.0fc83bda.png"
+  },
+  {
+    "revision": "c5c681d0278189cdde79b2d6fb603850",
+    "url": "/animal-flashcards/static/media/frog.c5c681d0.png"
+  },
+  {
+    "revision": "6b261573a0a8fe3176c3e80d2d6d001f",
+    "url": "/animal-flashcards/static/media/giraffe.6b261573.png"
+  },
+  {
+    "revision": "84089516aaa3982543c0e9aed80b6647",
+    "url": "/animal-flashcards/static/media/hedgehog.84089516.png"
+  },
+  {
+    "revision": "954492fedd4633b1e8441f2cf958fad9",
+    "url": "/animal-flashcards/static/media/kangaroo.954492fe.png"
+  },
+  {
+    "revision": "e2eb7f1cbb32b615d2b08d7e42467a9d",
+    "url": "/animal-flashcards/static/media/lion.e2eb7f1c.png"
+  },
+  {
+    "revision": "7aaef96670ccc61f55e1d30409c698e1",
+    "url": "/animal-flashcards/static/media/monkey.7aaef966.png"
+  },
+  {
+    "revision": "b059810b8bc419db5339a4e23ab23c5c",
+    "url": "/animal-flashcards/static/media/owl.b059810b.png"
+  },
+  {
+    "revision": "50e54265bff7cdcc0cdb483a0d8fd810",
+    "url": "/animal-flashcards/static/media/pandabear.50e54265.png"
+  },
+  {
+    "revision": "a8908f72227da5c612bc206bf6a47041",
+    "url": "/animal-flashcards/static/media/parrot.a8908f72.png"
+  },
+  {
+    "revision": "27bb20d348339276923b26ffd3f2381c",
+    "url": "/animal-flashcards/static/media/tiger.27bb20d3.png"
+  }
+]);
